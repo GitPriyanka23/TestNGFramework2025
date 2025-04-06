09316f8efb53d56e18601ccdb89368fd7830c936
GitPriyanka23 <dawanepriya.555@gmail.com>
2025-04-06 02:36:29 -0700

    ExcelRead file is added

---
+++ ExcelRead/ReadExcel.java
@@ -0,0 +1,72 @@
+package ExcelRead;
+
+import java.io.File;
+
+
+public class ReadExcel {
+
+	public static void main(String[] args) {
+		/*
+		 * 
+		 	Name if library for diffrent file type
+			Apche POI - read and write Excel file
+			CSV 	  -opencsv Jar	
+			JSON      -json-simple
+			XML       -simple java  classes	
+
+		 
+		 * for .xlsx-
+		 * Below all are class, hence we need to create an object-to access them
+		 * 
+		 *		 XSSFWorkbook- help to load the workbook
+		 *		 XSSFSheet-help to  load a sheet
+		 * 		 XSSSF-row- help to load a specific row
+		 * 		 XSSSFcell- load cell
+		 * 
+		 * 
+		 *  If you are using lder version .xls-
+		 *		 HSSFWorkbook- help to load the workbook
+		 *		 HSSFSheet-help to  load a sheet
+		 * 		 HSSSF-row- help to load a specific row
+		 * 		 HSSSFcell- load cell
+	
+		 */
+		
+		//step1 load the excel
+		
+		File src = new File("./TestData/Data.xlsx");
+		
+	//	XSSFWorkbook wb = new XSSFWorkbook(src);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
